627d0da64f1ff5e3f50ae72252ab3491e62f7056
Jeff Steward <steward.jeff@gmail.com>
2017-08-25 12:18:58 -0400

    update to clarifai v2 API

--- parsers/clarifai.py
+++ parsers/clarifai.py
@@ -1,11 +1,12 @@
 import config
 from clarifai.rest import ClarifaiApp
+from clarifai.rest import Image as ClImage
 
 class Clarifai(object):
 
 	def fetch(self, photo_file, id):
 
-		clarifai_api = ClarifaiApp(config.CLARIFAI_APP_ID, config.CLARIFAI_APP_SECRET)
+		clarifai_api = ClarifaiApp(api_key = config.CLARIFAI_API_KEY)
 		result = clarifai_api.models.get('general-v1.3').predict_by_url(photo_file)
 
 		return result
