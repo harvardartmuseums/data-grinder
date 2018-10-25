268857a41cb54d9921e1ac876e3782e9696d9451
Jeff Steward <steward.jeff@gmail.com>
2018-10-25 13:42:46 -0400

    update imagga api to v2

--- parsers/imagga.py
+++ parsers/imagga.py
@@ -8,16 +8,16 @@ class Imagga(object):
 		self.api_secret = config.IMAGGA_SECRET
 
 	def fetch(self, photo_file):
-		response = requests.get('https://api.imagga.com/v1/tagging?url=%s' % photo_file, auth=(self.api_key, self.api_secret))
+		response = requests.get('https://api.imagga.com/v2/tags?image_url=%s' % photo_file, auth=(self.api_key, self.api_secret))
 
 		return response.json()
 
 	def fetch_categories(self, photo_file):
-		response = requests.get('https://api.imagga.com/v1/categorizations/personal_photos?url=%s' % photo_file, auth=(self.api_key, self.api_secret))
+		response = requests.get('https://api.imagga.com/v2/categories/personal_photos?image_url=%s' % photo_file, auth=(self.api_key, self.api_secret))
 
 		return response.json()
 
 	def fetch_colors(self, photo_file):
-		response = requests.get('https://api.imagga.com/v1/colors?url=%s' % photo_file, auth=(self.api_key, self.api_secret))
+		response = requests.get('https://api.imagga.com/v2/colors?image_url=%s' % photo_file, auth=(self.api_key, self.api_secret))
 
 		return response.json()		
