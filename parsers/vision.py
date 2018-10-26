3e7c50fd0c0e7197d8f3e9d636608c0b1a416c5e
Jeff Steward <steward.jeff@gmail.com>
2018-10-26 11:16:40 -0400

    disable cache discovery for google apis

--- parsers/vision.py
+++ parsers/vision.py
@@ -18,7 +18,7 @@ class Vision(object):
         # [START authenticate]
         # credentials = GoogleCredentials.get_application_default()
         credentials = GoogleCredentials.from_stream(config.GOOGLE_VISION_CREDENTIALS_FILE)
-        service = discovery.build('vision', 'v1', credentials=credentials,
+        service = discovery.build('vision', 'v1', cache_discovery=False, credentials=credentials,
                                   discoveryServiceUrl=DISCOVERY_URL)
         # [END authenticate]
 
