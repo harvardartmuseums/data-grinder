21a5902765d51c62f7fed4390618a6ae0c87aa72
Jeff Steward <steward.jeff@gmail.com>
2019-03-01 16:45:49 -0500

    add error handling

--- parsers/clarifai.py
+++ parsers/clarifai.py
@@ -6,7 +6,11 @@ class Clarifai(object):
 
 	def fetch(self, photo_file, id):
 
-		clarifai_api = ClarifaiApp(api_key = config.CLARIFAI_API_KEY)
-		result = clarifai_api.models.get('general-v1.3').predict_by_url(photo_file)
-
+		try:
+			clarifai_api = ClarifaiApp(api_key = config.CLARIFAI_API_KEY)
+			result = clarifai_api.models.get('general-v1.3').predict_by_url(photo_file)
+		except Exception as e:
+			error =  json.loads(e.response.content)
+			print(error)
+			
 		return result
