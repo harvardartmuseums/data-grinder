e6ee8d647a0b43855b15f2221f1ce5605137f8d7
Jeff Steward <steward.jeff@gmail.com>
2019-05-28 17:24:19 -0400

    add config setting for AWS region

--- parsers/aws.py
+++ parsers/aws.py
@@ -6,9 +6,10 @@ class AWS(object):
     def __init__(self):
         self.aws_key = config.AWS_ACCESS_KEY
         self.aws_secret = config.AWS_SECRET_ACCESS_KEY
+        self.aws_region = 'us-east-1'
     
     def get_client(self):
-        return boto3.client('rekognition', aws_access_key_id=self.aws_key, aws_secret_access_key=self.aws_secret)
+        return boto3.client('rekognition', region_name=self.aws_region, aws_access_key_id=self.aws_key, aws_secret_access_key=self.aws_secret)
 
     def fetch_labels(self, photo_file):
         client = self.get_client()
