dfd457434531544b9685bb295a20f5a06cdbecb0
Jeff Steward <steward.jeff@gmail.com>
2023-10-12 10:31:13 -0400

    updated iiif qualitiy param

--- parsers/iiif.py
+++ parsers/iiif.py
@@ -6,7 +6,7 @@ class IIIFImage(object):
 		return 'https://ids.lib.harvard.edu/ids/iiif/%s' % id
 
 	def get_full_image_url(id):
-		return 'https://ids.lib.harvard.edu/ids/iiif/%s/full/full/0/native.jpg' % id
+		return 'https://ids.lib.harvard.edu/ids/iiif/%s/full/full/0/default.jpg' % id
 
 	def fetch(self, iiifImageURI):
 		response = requests.get('%s/info.json' % iiifImageURI)
